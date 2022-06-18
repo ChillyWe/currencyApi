7a687f3b416082a11c11358773d4451e2e7f9411
drupkin <d.rupkin@efbet.com>
2022-06-18 11:39:44 +0300

    mariadb docker compose file added add base scan

--- src/main/java/bg/dr/chilly/currencyApi/CurrencyApiApplication.java
+++ src/main/java/bg/dr/chilly/currencyApi/CurrencyApiApplication.java
@@ -3,7 +3,7 @@ package bg.dr.chilly.currencyApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"bg.dr.chilly.currencyApi"})
 public class CurrencyApiApplication {
 
 	public static void main(String[] args) {
